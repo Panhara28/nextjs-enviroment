a61effb2acd064fd781d956b0312c6b4fece1c8b
Panhara28 <titpanhara@gmail.com>
2020-12-30 00:05:11 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -5,10 +5,12 @@
 module.exports = {
   serverRuntimeConfig: {
     // Will only be available on the server side
+    SERVER_API_URL: process.env.API_URL,
     BASE_URL_ONLY: process.env.BASE_URL_ONLY,
   },
   publicRuntimeConfig: {
     // Will be available on both server and client
     BASE_URL_BOTH: process.env.BASE_URL_BOTH,
+    BOTH_API_URL: process.env.BOTH_API_URL
   },
 };
--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,36 @@ import Head from 'next/head';
 import getConfig from 'next/config';
 
 import styles from '../styles/Home.module.css';
+import { gql, useQuery } from '@apollo/client';
 
 const {
   serverRuntimeConfig: { BASE_URL_ONLY } = {},
+  serverRuntimeConfig: { SERVER_API_URL } = {},
   publicRuntimeConfig: { BASE_URL_BOTH } = {},
 } = getConfig();
 
 console.log('serverRuntimeConfig>>>>  ', BASE_URL_ONLY);
 console.log('publicRuntimeConfig>>>>  ', BASE_URL_BOTH);
+console.log('SERVER_API_URL>>>>  ', SERVER_API_URL);
 
 export default function Home() {
+  const { data, loading } = useQuery(gql`
+    query tagList{
+      tagList{
+        data{
+          id
+        }
+      }
+    }
+  `)
+  if(loading) return <div>Loading...</div>
+  console.log(data);
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h2>{`BASE_URL_ONLY: ${BASE_URL_ONLY || ''}`}</h2>
         <h2>{`BASE_URL_BOTH: ${BASE_URL_BOTH || ''}`}</h2>
+        <h2>{`API_URL: ${API_URL || ''}`}</h2>
       </main>
     </div>
   );
