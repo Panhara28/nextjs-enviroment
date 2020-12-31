95b40ad3b3bd3754f5d482f7c88ca3a5bb67754d
Panhara28 <titpanhara@gmail.com>
2020-12-31 22:10:34 +0700

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,15 @@ import getConfig from 'next/config';
 import styles from '../styles/Home.module.css';
 import { gql, useQuery } from '@apollo/client';
 
-const {
-  publicRuntimeConfig: { API_FROM_CLIENT } = {},
-  publicRuntimeConfig: { API_FROM_CLIENT_SERVER } = {},
-  serverRuntimeConfig: { API_FROM_SERVER } = {},
-} = getConfig();
+// const {
+//   publicRuntimeConfig: { API_FROM_CLIENT } = {},
+//   publicRuntimeConfig: { API_FROM_CLIENT_SERVER } = {},
+//   serverRuntimeConfig: { API_FROM_SERVER } = {},
+// } = getConfig();
 
-console.log('API_FROM_CLIENT_ONLY >>>>  ', API_FROM_CLIENT);
-console.log('API_FROM_CLIENT_SERVER >>>>  ', API_FROM_CLIENT_SERVER);
-console.log('API_FROM_SERVER_ONLY >>>>  ', API_FROM_SERVER);
+// console.log('API_FROM_CLIENT_ONLY >>>>  ', API_FROM_CLIENT);
+// console.log('API_FROM_CLIENT_SERVER >>>>  ', API_FROM_CLIENT_SERVER);
+// console.log('API_FROM_SERVER_ONLY >>>>  ', API_FROM_SERVER);
 
 export default function Home() {
   const { data, loading } = useQuery(gql`
@@ -31,9 +31,9 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h2>{`API_FROM_CLIENT_ONLY: ${API_FROM_CLIENT || ''}`}</h2>
+        {/* <h2>{`API_FROM_CLIENT_ONLY: ${API_FROM_CLIENT || ''}`}</h2>
         <h2>{`API_FROM_CLIENT_SERVER: ${API_FROM_CLIENT_SERVER || ''}`}</h2>
-        <h2>{`API_FROM_SERVER_ONLY: ${API_FROM_SERVER || ''}`}</h2>
+        <h2>{`API_FROM_SERVER_ONLY: ${API_FROM_SERVER || ''}`}</h2> */}
       </main>
     </div>
   );
